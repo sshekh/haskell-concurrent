75b18a29e0b7fa0c510495b5dd5c3d54f3752cbb
Irfan Hudda <irfanhudda@gmail.com>
2015-11-13 02:53:27 +0530

    adding command line arguments for test

--- lockfree-queue/Test/ConcurrentTest.hs
+++ lockfree-queue/Test/ConcurrentTest.hs
@@ -1,6 +1,6 @@
 -- | concurrent test
 
-module Test.ConcurrentTest where
+module Main where
 
 import Test.Checks
 import Test.QuickCheck
@@ -16,9 +16,11 @@ import System.Environment
 main :: IO()
 main = do
   x <- getArgs
-  print x
-  runThreadOps2 2500000 4
-  putStrLn "Done"
+  let n = read $ x!!0
+      m = read $ x!!1
+  -- print x
+  runThreadOps2 n m
+  -- putStrLn "Done"
 
 
 
@@ -32,13 +34,25 @@ forkThread proc = do
 runThreadOps2 :: Int -> Int -> IO ()
 runThreadOps2 m n = do
   a <- newq
-  let enqNTimes a n v = do mapM (\x -> if v `mod` 2 == 1
-                                         then enq a x
-                                         else do deq a
-                                                 return ()) [1..n]
+  let enqNTimes a n v = do mapM (\x -> if x `mod` 10 < 5 then enq a x
+                                        else do deq a
+                                                return ()) [1..n]
                            return ()
   threads <- forM [1..n] (\v -> forkThread (enqNTimes a m v))
   mapM_ takeMVar threads
   x <- queueList a
   -- print x
-  print (length x)
+  -- print (length x)
+  return ()
+
+runThreadOps3 :: Int -> Int -> IO ()
+runThreadOps3 m n = do
+  a <- newq
+  let enqNTimes a n v = do mapM (\x -> if v `mod` 2 == 1 then enq a x
+                                        else do deq a
+                                                return ()) [1..n]
+                           return ()
+  threads <- forM [1..n] (\v -> forkThread (enqNTimes a m v))
+  mapM_ takeMVar threads
+  x <- queueList a
+  return ()
